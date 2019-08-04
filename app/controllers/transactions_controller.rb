a53ee5f492ec28a23ed761cf9b24dfa2aba31c5d
Armin Primadi <aprimadi@gmail.com>
2019-08-04 15:40:24 +0700

    Implementation of transactions controller

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,10 +1,51 @@
 class TransactionsController < ApplicationController
   def deposit
+    wallet = Wallet.find_by(id: params[:wallet_id])
+    amount = params[:amount].to_i
+
+    return api_error("Wallet not found") if wallet.nil?
+    return api_error("Invalid amount") if amount <= 0
+
+    begin
+      tm = TransactionManager.instance
+      tm.deposit(wallet, amount)
+      api_success
+    rescue StandardError => e
+      api_error(e.message)
+    end
   end
 
   def withdraw
+    wallet = Wallet.find_by(id: params[:wallet_id])
+    amount = params[:amount].to_i
+
+    return api_error("Wallet not found") if wallet.nil?
+    return api_error("Invalid amount") if amount <= 0
+
+    begin
+      tm = TransactionManager.instance
+      tm.withdraw(wallet, amount)
+      api_success
+    rescue StandardError => e
+      api_error(e.message)
+    end
   end
 
   def transfer
+    source_wallet = Wallet.find_by(id: params[:source_wallet_id])
+    target_wallet = Wallet.find_by(id: params[:target_wallet_id])
+    amount = params[:amount].to_i
+
+    return api_error("Source wallet not found") if source_wallet.nil?
+    return api_error("Target wallet not found") if target_wallet.nil?
+    return api_error("Invalid amount") if amount <= 0
+
+    begin
+      tm = TransactionManager.instance
+      tm.transfer(source_wallet, target_wallet, amount)
+      api_success
+    rescue StandardError => e
+      api_error(e.message)
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  resources :entities, only: [:index, :create]
+  resources :transactions, only: [] do
+    collection do
+      post :deposit
+      post :withdraw
+      post :transfer
+    end
+  end
+
   root 'landing#index'
 end
